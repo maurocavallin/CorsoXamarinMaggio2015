0951f0f2888db7805ec954885d59fb115476ee54
Lemcube <amministrazione@lemcube.com>
2015-05-13 15:39:33 +0200

    riportato il codice del modulo 2 su modulo 3

---
+++ Modulo3/CorsoXamarinModulo3/SecondaPagina.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CorsoXamarin
+{
+	public partial class SecondaPagina : ContentPage
+	{
+		public SecondaPagina ()
+		{
+			InitializeComponent ();
+
+			Title = "Pagina Principale";
+		}
+	}
+}
+
