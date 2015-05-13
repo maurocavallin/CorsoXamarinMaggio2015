669dc510e313e67133274f17c3577976c8407c38
Lemcube <amministrazione@lemcube.com>
2015-05-13 15:09:18 +0200

    [UPD] pagine di login (senza un progetto, solo pagine sparse) con login, masterdetail, menu con lista

---
+++ Modulo2/PagineXamarinForms/DetailPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CorsoXamarin
+{
+	public partial class DetailPage : ContentPage
+	{
+		public DetailPage ()
+		{
+			InitializeComponent ();
+			BackgroundColor = Color.Gray;
+
+			this.Title = "Applicazione";
+		}
+	}
+}
+
---
+++ Modulo2/PagineXamarinForms/MainMarsterDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace CorsoXamarin
+{
+	public class MainMarsterDetail : MasterDetailPage
+	{
+		LoginPage _loginpage;
+
+		public MainMarsterDetail ()
+		{
+			Master = new MenuPage ();
+			Detail = new DetailPage ();
+
+			_loginpage = new LoginPage ();
+		}
+
+		protected override void OnAppearing ()
+		{
+			base.OnAppearing ();
+
+			if (!_loginpage.SeLoggato) {
+				Navigation.PushModalAsync (_loginpage);
+			}
+		}
+	}
+}
+
+
