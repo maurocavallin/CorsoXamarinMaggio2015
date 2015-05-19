313b43b47d7b819f72afbfa8ef92a5b146bbe8bb
Mauro Cavallin <mauro.cavallin@lemcube.com>
2015-05-19 18:27:15 +0200

    aggiunti file mancanti esempio accesso ai dati

---
+++ Modulo4/EsempioAccessoDatiLocali/EsempioAccessoDatiLocali/EsempioAccessoDatiLocali.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace EsempioAccessoDatiLocali
+{
+	public class App : Application
+	{
+		// http://developer.xamarin.com/guides/cross-platform/application_fundamentals/data/
+		// https://github.com/xamarin/mobile-samples/tree/master/DataAccess
+
+		// http://developer.xamarin.com/guides/cross-platform/application_fundamentals/data/part_2_configuration/
+
+		static TodoItemDatabase database;
+
+		public static TodoItemDatabase Database {
+			get { 
+				if (database == null) {
+					database = new TodoItemDatabase ();
+				}
+				return database; 
+			}
+		}
+
+
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new PaginaMenuPrincipale();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
